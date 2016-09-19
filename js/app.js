8eb6bb888dfb3960ba3b2ef995161a49bb0c11cd
nalgritz <nalgritz9270@gmail.com>
2016-09-19 23:23:43 +0800

    initial commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+var app = angular./**
+* hackerNewsApp Module
+*
+* Description
+*/
+angular.module('hackerNewsApp', []).
---
+++ js/controllers/posts_controller.js
@@ -0,0 +1,6 @@
+app.controller('PostsCtrl', ['$scope', function($scope){
+
+// newsfeed link
+// "http://hn.algolia.com/api/v1/search_by_date?query=angular"
+
+}])
