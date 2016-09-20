e714865b9e3662059932eeeba44bdd57f75fd5b4
nalgritz <nalgritz9270@gmail.com>
2016-09-20 23:53:41 +0800

    completed mvp

--- js/controllers/posts_controller.js
+++ js/controllers/posts_controller.js
@@ -1,14 +1,9 @@
 app.controller('PostsCtrl', ['$scope', '$http', 'GetPosts', function($scope, $http, GetPosts){
 
-  $scope.posts = GetPosts.query().sort(function (a, b) {return a.created_at > b.created_at});
-
-  // $scope.clickPost = function ($index) {
-  //   $('#posts-container').on('click', '.post-item', function () {
-
-  //   })
-  // }
+  $scope.posts = GetPosts.query();
 
   $scope.deletePost = function ($index) {
     $scope.posts.splice($index, 1);
   }
+
 }])
