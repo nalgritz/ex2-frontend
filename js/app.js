62732fa75be6f7d9cc095d01c0f88e0f101dea47
nalgritz <nalgritz9270@gmail.com>
2016-09-22 00:23:10 +0800

    clean up

--- js/app.js
+++ js/app.js
@@ -4,14 +4,3 @@ var app = /**
 * Description
 */
 angular.module('hackerNewsApp', ['ui.bootstrap', 'ngResource', 'angularMoment']);
-
-// function setData (e) {
-//   for (i = 0; i < e.length; i++) {
-//     if (e[i].title) {
-//       e[i].story_title = angular.copy(e[i].title);
-//       e[i].story_url = angular.copy(e[i].url);
-//     } else if (e[i].story_title) {} else {
-//       e.splice(i, 1);
-//     }
-//   }
-// };
