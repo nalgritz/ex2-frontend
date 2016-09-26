35e68428053c2ab5c60ec8e6bff2fe4579a38b00
nalgritz <nalgritz9270@gmail.com>
2016-09-27 00:59:00 +0800

    try out hide spinner after loading

--- js/controllers/posts_controller.js
+++ js/controllers/posts_controller.js
@@ -1,6 +1,10 @@
-app.controller('PostsCtrl', ['$scope', '$http', 'GetPosts', function($scope, $http, GetPosts){
+app.controller('PostsCtrl', ['$scope', 'GetPosts', function($scope, GetPosts){
 
   $scope.posts = GetPosts.query();
+// this is not a promise, doesn't work
+// .then(function () {
+//     console.log('hi');
+//   });
 
   $scope.deletePost = function ($index) {
     $scope.posts.splice($index, 1);
--- js/resources/posts_factory.js
+++ js/resources/posts_factory.js
@@ -1,4 +1,18 @@
-app.factory('GetPosts', ['$resource', 'setData', function($resource, setData){
+app.factory('GetPosts', ['$resource', function($resource){
+  function setData(e){
+    for (i = 0; i < e.length; i++) {
+      if (e[i].title) {
+        e[i].story_title = angular.copy(e[i].title);
+      } else if (e[i].story_title) {} else {
+        e.splice(i, 1);
+      };
+      if (e[i].url) {
+        e[i].story_url = angular.copy(e[i].url);
+      };
+    }
+    return e;
+  };
+
   return $resource("http://hn.algolia.com/api/v1/search_by_date?query=angular", {}, {
     query: {
       method: 'GET',
